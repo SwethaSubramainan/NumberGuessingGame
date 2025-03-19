b8debd5d9c0f7d54d3ea5f4e5819d9089a2ecff5
SwethaSubramainan <swethavaishu2713@gmail.com>
2025-03-19 18:59:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var startButton=document.getElementById("start");
+
+startButton.onclick=()=>{
+    location.href="./game.html";
+};
+
+
+
+
+
+// function sum(a,b)
+// {
+//     return a+b;
+// }
+// var sum=(a,b)=>{
+
+// };
